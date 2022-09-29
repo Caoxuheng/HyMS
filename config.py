0146425f597ed5b23b3885742d48ee825d953116
Caoxuheng <80499410+Caoxuheng@users.noreply.github.com>
2022-09-29 20:53:18 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--path',type=str,default='', help='where you store your HSI data file')
-parser.add_argument('--save_path',type=str,default='', help='where you store your HSI data file')
-parser.add_argument('--sf',type=int,default=32, help='scale_factor, set to 8, 16, and 32 in our experiment')
-parser.add_argument('--d',type=int, default=2,help='search scale')
-parser.add_argument("--beta", type=float, default=1e-4,help='regularization parameter beta 𝛽')
-parser.add_argument("--gamma", type=float, default=1e-6,help='plenty parameter beta γ ')
-args=parser.parse_args()
